b8ec45e942682a35dede5f98690905a362d5437c
yys12138 <1623656521@qq.com>
2021-08-24 15:03:45 +0800

    weibo

--- weibo/settings.py
+++ weibo/settings.py
@@ -12,7 +12,7 @@ DEFAULT_REQUEST_HEADERS = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'your cookie'
+    'cookie': '_T_WM=82689783695; SCF=AkmB5XwoF6fjk-xMGCAgCDtsG8K4D8OPgKtAkHT_EG5DkCnXGh0ejo4lKFBPFnKsqI6KxFl9T0-z6KqkR73GZw8.; SUB=_2A25MIPvDDeRhGeNG6lsU-CbMyTmIHXVv6oWLrDV6PUJbktCOLWz6kW1NS2Ius0EhOCnhLkLccWb0r-ztXGz67wSv; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WW7PI-x.h46o4xd2mKXRMzd5NHD95Qf1h24SKnRehzfWs4DqcjZIrH0UGULMcyaU7tt; SSOLoginState=1629784980; MLOGIN=1; M_WEIBOCN_PARAMS=luicode%3D20000174; WEIBOCN_FROM=1110106030'
 }
 ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
@@ -24,18 +24,18 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['迪丽热巴']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['TCL电视']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
-WEIBO_TYPE = 1
+WEIBO_TYPE = 0
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
 CONTAIN_TYPE = 0
 # 筛选微博的发布地区，精确到省或直辖市，值不应包含“省”或“市”等字，如想筛选北京市的微博请用“北京”而不是“北京市”，想要筛选安徽省的微博请用“安徽”而不是“安徽省”，可以写多个地区，
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用”全部“
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2020-03-01'
+START_DATE = '2021-08-01'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2020-03-01'
+END_DATE = '2021-08-24'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
 FURTHER_THRESHOLD = 46
